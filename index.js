17c3e459f8ce03e281846a8f00d4ef902869a381
Taylor Shuff <tkylesh@gmail.com>
2016-08-15 20:34:39 -0500

    added some more css and mapped out the javascript

---
+++ index.js
@@ -0,0 +1,28 @@
+//task1: insert card into dom
+
+
+
+
+//task2: add two color inputs above text
+//on each card.  one for text color, the
+//other for background-color
+
+
+//when user selects a background-color
+//change that to background color of 
+//card
+
+//when user selects font color
+//change that to font color on card
+
+
+
+//task3: add text from text area to card
+
+
+
+
+
+
+//add card with all of above when user
+//clicks on "create card" button
